1103e0d2e0a0f02bfebbb7e80e919de9b92992e9
dtdavid <dtdavidMacMini@DtdavidMacMini.local>
2025-02-17 22:28:22 +0800

    Configured root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,7 @@
-// Root-level build.gradle.kts
-
 buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
     }
 }
 
